bef8bf0f18760765c20641f356c2f2d84aac7a47
zhuazhu <850084453@qq.com>
2020-06-03 15:14:52 +0800

    test

--- spring/src/main/java/com/example/demo/controller/testController.java
+++ spring/src/main/java/com/example/demo/controller/testController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = {"/test"})
-public class testController {
+@RequestMapping
+public class testController{
     @RequestMapping
     public String test(){
         return "hi";
