681e7e3a80bd827988fcd76b7ab97de769cc130b
zhuazhu <850084453@qq.com>
2020-06-05 22:25:33 +0800

    test demo

--- spring/src/main/java/com/example/demo/controller/testController.java
+++ spring/src/main/java/com/example/demo/controller/testController.java
@@ -1,13 +1,42 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dao.mapper.TestModelMapper;
+import com.example.demo.dao.model.TestModel;
+import com.example.demo.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = {"/test"})
 public class testController{
-    @RequestMapping
-    public String test(){
-        return "hi";
+    @Autowired
+    TestService testService;
+    @Autowired
+    TestModelMapper testModelMapper;
+    @PostMapping(value = {"/"})
+    public ResponseEntity<TestModel> insert(@RequestBody TestModel testModel) throws Exception {
+        TestModel result = testService.insert(testModel);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+    @DeleteMapping(value = {"/"})
+    public ResponseEntity<Integer> Delete(@RequestParam("tid") Long id) throws Exception {
+        Integer result = testService.delete(id);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PutMapping(value = {"/"})
+    public ResponseEntity<TestModel> update(@RequestBody TestModel testModel) throws Exception {
+        TestModel result = testService.update(testModel);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+    @GetMapping(value = {"/"})
+    public ResponseEntity<List<TestModel>> selectAll()  {
+        List<TestModel> result = testService.search();
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
